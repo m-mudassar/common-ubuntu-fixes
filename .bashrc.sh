b6382486c06a759a9c841b39fd897f3b3ceb2383
mudassar <mimj1800@gmail.com>
2022-10-25 18:06:23 +0500

    initial commit

---
+++ .bashrc.sh
@@ -0,0 +1,31 @@
+# The purpose of this guide is to 
+
+
+# The point of this helper is to reduce the number of times you must
+# type your username or password. For example:
+git config credential.helper store
+git push http://example.com/repo.git
+Username: <type your username>
+Password: <type your password>
+# several days later use
+# your credentials are used automatically
+git push http://example.com/repo.git
+
+# exporting flutter patch
+export PATH="$PATH:/flutter/bin"
+
+# Show git branch name
+force_color_prompt=yes
+color_prompt=yes
+parse_git_branch() {
+ git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+if [ "$color_prompt" = yes ]; then
+ PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033>
+else
+ PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w$(parse_git_branch)\$ '
+fi
+unset color_prompt force_color_prompt
+
+
+
